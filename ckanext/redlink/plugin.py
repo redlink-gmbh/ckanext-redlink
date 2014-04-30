44887afd25b34b95a0eb0b1b73f82172310d12ac
David Riccitelli <david@insideout.io>
2014-04-30 13:38:19 +0300

    add a Redlink Dataset field with the dataset name on Redlink

--- ckanext/redlink/plugin.py
+++ ckanext/redlink/plugin.py
@@ -1,72 +1,48 @@
-import logging
 import ckan.plugins as p
+import ckan.plugins.toolkit as tk
+import logging
 import urllib
 import urllib2
-import ckan.plugins.toolkit as toolkit
-
 
 from pylons import config
 
 log = logging.getLogger(__name__)
 
-try:
-    import ckanext.resourceproxy.plugin as proxy
-except ImportError:
-    pass
-
 
 class RedlinkPreview(p.SingletonPlugin):
-    """This extension previews PDFs. """
+    """This extension previews data hosted on Redlink. """
     p.implements(p.IConfigurer, inherit=True)
-    p.implements(p.IConfigurable, inherit=True)
     p.implements(p.IResourcePreview, inherit=True)
     p.implements(p.IRoutes, inherit=True)
 
+    redlink_dataset = ''
     supported_formats = ['redlink', 'squebi']
-    app_key = 'AAA'
-    proxy_is_enabled = False
 
+
+    ## IConfigurer
     def update_config(self, config):
         p.toolkit.add_public_directory(config, 'theme/public')
         p.toolkit.add_template_directory(config, 'theme/templates')
         p.toolkit.add_resource('theme/public', 'ckanext-redlink')
 
-    def configure(self, config):
-        """Get the application key."""
-        app_key = config.get('redlink.app.key', '')
-
-        log.info('configure [ app key :: {0} ]'.format(app_key))
-
-        self.app_key = app_key
-
-        enabled = config.get('ckan.resource_proxy_enabled', False)
-        self.proxy_is_enabled = enabled
 
     def can_preview(self, data_dict):
         resource = data_dict['resource']
         format_lower = resource['format'].lower()
         if format_lower in self.supported_formats:
             return {'can_preview': True, 'quality': 2}
-            # if resource['on_same_domain'] or self.proxy_is_enabled:
-            #     return {'can_preview': True, 'quality': 2}
-            # else:
-            #     return {'can_preview': False,
-            #             'fixable': 'Enable resource_proxy',
-            #             'quality': 2}
+
         return {'can_preview': False}
 
-    def setup_template_variables(self, context, data_dict):
-        if (self.proxy_is_enabled
-                and not data_dict['resource']['on_same_domain']):
-            url = proxy.get_proxified_resource_url(data_dict)
-            p.toolkit.c.resource['url'] = url
 
     def preview_template(self, context, data_dict):
+        RedlinkPreview.redlink_dataset = data_dict['resource']['redlink_dataset']
+        log.info('preview_template [ dataset :: {} ]'.format(RedlinkPreview.redlink_dataset))
         return 'redlink.html'
+
 
     ## IRoutes
     def after_map(self, map):
-
         controller = 'ckanext.redlink.plugin:RedlinkController'
         map.connect('/redlink', controller=controller, action='redlink')
 
@@ -74,9 +50,7 @@ class RedlinkPreview(p.SingletonPlugin):
 
 
 class RedlinkController(p.toolkit.BaseController):
-
     def redlink(self, environ):
-
         # The client may request different response formats. The format is set in the Accept header.
         # We get the Accept header from the request and copy it to the request we relay to the remote
         # server.
@@ -86,12 +60,16 @@ class RedlinkController(p.toolkit.BaseController):
 
         app_key = config.get('redlink.app.key', '')  # Get the application key set in the configuration.
 
-        url = 'https://api.redlink.io/1.0-BETA/data/events/sparql/select?key=' + app_key
+        dataset = RedlinkPreview.redlink_dataset
+        log.info('redlink [ dataset :: {} ]'.format(dataset))
+        url = 'https://api.redlink.io/1.0-BETA/data/' + dataset + '/sparql/select?key=' + app_key
+
+        log.info('Posting request [ url :: {} ]'.format(url))
 
         # The client is sending the SPARQL statement as the request body. Since we're going to send a
         # url-encoded POST, we need to copy it to the query key.
 
-        values = {'query': toolkit.request.body}
+        values = {'query': tk.request.body}
 
         data = urllib.urlencode(values)
         req = urllib2.Request(url, data, headers)
